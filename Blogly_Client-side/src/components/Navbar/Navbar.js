0f111d3d4bb91d0182852d2cb839107da747eee7
niiyazobug <niiyaz5555@gmail.com>
2022-06-23 17:06:31 +0600

    dark light them toggle method added

--- Blogly_Client-side/src/components/Navbar/Navbar.js
+++ Blogly_Client-side/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../style/style.css'
 
-const Navbar = ({children}) => {
+const Navbar = ({ children }) => {
     return (
         <div className="drawer drawer-end">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
@@ -12,23 +12,44 @@ const Navbar = ({children}) => {
                     <div>
                         <img src="https://i.ibb.co/RNGY4SB/logo-nav.png" alt="" className='w-[37px]' />
                     </div>
-                <div className="flex-1 px-2 mx-2 text-2xl tracking-widest font-semibold montserrat-alternates">blogly</div>
+                    <div className="flex-1 px-2 mx-2 text-2xl tracking-widest font-semibold montserrat-alternates">blogly</div>
                     <div className="flex-none lg:hidden">
                         <label for="my-drawer-3" className="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </label>
                     </div>
-                    
+
                     <div className="flex-none hidden lg:block">
                         <ul className="menu menu-horizontal gap-6 montserrat-alternates font-semibold tracking-wide">
 
                             {/* <!-- Navbar menu content here --> */}
-                            <Link to='/'>Home</Link>
-                            <Link to='/allblog'>All Blogs</Link>
-                            <Link to='/addblog'>Add Blogs</Link>
-                            {/* <Link to='/about'>About</Link> */}
-                            <Link to='/signin'>Sign In</Link>
-                            <Link to='/signout'>Sign Out</Link>
+                            <li>
+                                <button data-toggle-theme="wireframe,black" data-act-class="ACTIVECLASS">Theam</button>
+                            </li>
+
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+
+                            <li>
+                                <Link to='/allblog'>All Blogs</Link>
+                            </li>
+
+                            <li>
+                                <Link to='/addblog'>Add Blogs</Link>
+                            </li>
+
+                            <li>
+                                {/* <Link to='/about'>About</Link> */}
+                            </li>
+
+                            <li>
+                                <Link to='/signin'>Sign In</Link>
+                            </li>
+
+                            <li>
+                                <Link to='/signout'>Sign Out</Link>
+                            </li>
 
                         </ul>
                     </div>
@@ -44,12 +65,34 @@ const Navbar = ({children}) => {
                 <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 montserrat-alternates font-semibold tracking-wide">
 
                     {/* <!-- Sidebar content here --> */}
-                    <Link to='/'>Home</Link>
-                    <Link to='/allblog'>All Blogs</Link>
-                    <Link to='/addblog'>Add Blogs</Link>
-                    {/* <Link to='/about'>About</Link> */}
-                    <Link to='/signin'>Sign In</Link>
-                    <Link to='/signout'>Sign Out</Link>
+                    <li>
+                        <button data-toggle-theme="dark,light" data-act-class="ACTIVECLASS">Theam</button>
+                    </li>
+
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+
+                    <li>
+                        <Link to='/allblog'>All Blogs</Link>
+                    </li>
+
+                    <li>
+                        <Link to='/addblog'>Add Blogs</Link>
+                    </li>
+
+                    <li>
+                        {/* <Link to='/about'>About</Link> */}
+                    </li>
+
+                    <li>
+                        <Link to='/signin'>Sign In</Link>
+                    </li>
+
+                    <li>
+                        <Link to='/signout'>Sign Out</Link>
+                    </li>
+
 
                 </ul>
 
--- Blogly_Client-side/tailwind.config.js
+++ Blogly_Client-side/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
-  theme: {
-    extend: {},
+
+  daisyui: {
+    themes: [
+      "wireframe",
+      "black",
+    ],
   },
+
   plugins: [
     require("daisyui"),
     require('tw-elements/dist/plugin')
