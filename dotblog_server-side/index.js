48347fdbd597fcaa69e0b573dec84098c816643c
niiyazobug <niiyaz5555@gmail.com>
2022-06-25 05:14:59 +0600

    blog post api done

--- dotblog_server-side/index.js
+++ dotblog_server-side/index.js
@@ -1,28 +1,30 @@
-const app = require('express')()
+const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// const app = express()
+const app = express()
 const port = process.env.PORT || 5000
 
 // middlewar
 app.use(cors())
-// app.use(express.json())
+app.use(express.json())
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynlrp.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynlrp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect()
-        const allBlogsCollection = client.db("test").collection("all-blogs");
+        const allBlogsCollection = client.db("dotblog-data-collection").collection("all-blogs");
 
-
-        // app.post('/blogs', async (req, res) => {
-        //     const data = req.body
-        // })
+      
+        // blog post api
+        app.post('/post-blog', async (req, res) => {
+            const postBlog = await allBlogsCollection.insertOne(req.body)
+            res.send(postBlog)
+        })
 
     }
     finally { }
